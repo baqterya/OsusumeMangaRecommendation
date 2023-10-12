07d443601ef65c431182ff15766c6ff6f274c74c
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-10-12 16:42:37 +0200

    navigation setup

--- app/src/main/java/com/baqterya/mangarecommendation/MainActivity.kt
+++ app/src/main/java/com/baqterya/mangarecommendation/MainActivity.kt
@@ -3,18 +3,11 @@ package com.baqterya.mangarecommendation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.baqterya.mangarecommendation.ui.theme.MangaRecommendationTheme
-import com.baqterya.mangarecommendation.ui.theme.mainmenu.MainMenuScreen
+import com.baqterya.mangarecommendation.ui.mainmenu.MainMenuScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     startDestination = "main_menu_screen",
                 ) {
                     composable("main_menu_screen") {
-                        MainMenuScreen(/*navController*/)
+                        MainMenuScreen(navController)
                     }
                 }
             }
