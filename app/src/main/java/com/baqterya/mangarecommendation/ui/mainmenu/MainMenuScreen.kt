66cee8213bfc92a74c2ae693327e5eb70721c3d1
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-10-12 16:54:41 +0200

    removed unnecessary import

--- app/src/main/java/com/baqterya/mangarecommendation/ui/mainmenu/MainMenuScreen.kt
+++ app/src/main/java/com/baqterya/mangarecommendation/ui/mainmenu/MainMenuScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
