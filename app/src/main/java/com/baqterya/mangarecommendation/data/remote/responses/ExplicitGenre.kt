05c8af8b2a5eb5357c7011dc2fbd6d51c4c81dee
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-10-24 17:24:27 +0200

    corrected responses classes so their field names correspond to the fetched json insead of kotlin naming conventions

--- app/src/main/java/com/baqterya/mangarecommendation/data/remote/responses/ExplicitGenre.kt
+++ app/src/main/java/com/baqterya/mangarecommendation/data/remote/responses/ExplicitGenre.kt
@@ -1,7 +1,7 @@
 package com.baqterya.mangarecommendation.data.remote.responses
 
 data class ExplicitGenre(
-    val malId: Int,
+    val mal_id: Int,
     val name: String,
     val type: String,
     val url: String
--- app/src/main/java/com/baqterya/mangarecommendation/data/remote/responses/Webp.kt
+++ app/src/main/java/com/baqterya/mangarecommendation/data/remote/responses/Webp.kt
@@ -1,7 +1,7 @@
 package com.baqterya.mangarecommendation.data.remote.responses
 
 data class Webp(
-    val imageUrl: String,
-    val largeImageUrl: String,
-    val smallImageUrl: String
+    val image_url: String,
+    val small_image_url: String,
+    val large_image_url: String
 )
