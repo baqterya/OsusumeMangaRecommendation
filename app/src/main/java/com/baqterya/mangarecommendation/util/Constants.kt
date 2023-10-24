ab8274805dd73ae55f621be3252833cd52c0dcd4
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-10-24 17:23:47 +0200

    corrected the url constant

--- app/src/main/java/com/baqterya/mangarecommendation/util/Constants.kt
+++ app/src/main/java/com/baqterya/mangarecommendation/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.baqterya.mangarecommendation.util
 
 object Constants {
-    const val BASE_URL = "https://api.jikan.moe/v4/manga/"
+    const val BASE_URL = "https://api.jikan.moe/v4/"
 }
