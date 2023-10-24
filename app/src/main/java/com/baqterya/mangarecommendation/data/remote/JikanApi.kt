975fcbba852d98929bc1fa194bc00b24332935f3
Jakub Jędros <43807907+baqterya@users.noreply.github.com>
2023-10-24 16:42:18 +0200

    added basic api functionality and repository class

--- app/src/main/java/com/baqterya/mangarecommendation/data/remote/JikanApi.kt
+++ app/src/main/java/com/baqterya/mangarecommendation/data/remote/JikanApi.kt
@@ -1,4 +1,14 @@
 package com.baqterya.mangarecommendation.data.remote
 
+import com.baqterya.mangarecommendation.data.remote.responses.Manga
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface JikanApi {
+
+    @GET("manga/{id}")
+    suspend fun getManga(
+        @Path("id") id: Int
+    ): Manga
+
 }
---
+++ app/src/main/java/com/baqterya/mangarecommendation/repository/MangaRepository.kt
@@ -0,0 +1,23 @@
+package com.baqterya.mangarecommendation.repository
+
+import com.baqterya.mangarecommendation.data.remote.JikanApi
+import com.baqterya.mangarecommendation.data.remote.responses.Manga
+import com.baqterya.mangarecommendation.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+
+@ActivityScoped
+class MangaRepository @Inject constructor(
+    private val api: JikanApi
+) {
+
+    suspend fun getManga(mangaId: Int): Resource<Manga> {
+        val response = try {
+            api.getManga(mangaId)
+        } catch (e: Exception) {
+            return Resource.Error(message="An unknown error occurred while fetching data.")
+        }
+        return Resource.Success(response)
+    }
+
+}
